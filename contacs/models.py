70b1149f60a62088e11d65691009c1c271a3faf9
Brian Quinlan <b_quinny@hotmail.com>
2018-11-08 12:06:54 +0000

    finished contacts app with add and edit

---
+++ contacs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=254, blank=False)
+    email = models.EmailField(max_length=254,blank=False)
+    
+    def __str__(self):
+        return self.name
---
+++ contacs/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Contact
+from .forms import ContactForm
+
+def say_hello(request):
+    contact = Contact.objects.all()
+    return render(request, "contacts/index.html", {'contact':contact})
+
+def add_contact(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        form.save()
+        return redirect("/")
+    else:    
+        form = ContactForm()
+        return render(request, "contacts/add_contact.html", {'form': form })
+
+
+  
+def edit_contact(request, id):
+    contact = Contact.objects.get(pk=id)
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        form.save()
+        return redirect("/")
+    else:        
+        form=ContactForm(instance=contact)
+        return render(request, "contacts/add_contact.html", {'form': form })
